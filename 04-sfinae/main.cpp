cd56393121eac5c986c72ae14544fd3aed18c4d2
Oleksii Ruban <alexey.ruban@ringteam.com>
2022-04-26 18:47:19 +0300

    Documentation

--- 04-sfinae/main.cpp
+++ 04-sfinae/main.cpp
@@ -4,6 +4,10 @@
 #include <type_traits>
 #include <vector>
 
+/*
+ * Output operator for printing two types of containers: list and vector
+ * By default puts comma between elements
+ */
 template<
     template<typename, typename> typename Container,
     typename Type,
@@ -67,7 +71,7 @@ int main() {
     print_ip( std::string{"Hello, World!"} ); // Hello, World!
     print_ip( std::vector<int>{100, 200, 300, 400} ); // 100.200.300.400
     print_ip( std::list<short>{400, 300, 200, 100} ); // 400.300.200.100
-    print_ip( std::make_tuple(123, 456, 789, 0) ); // 123.456.789.0
+//    print_ip( std::make_tuple(123, 456, 789, 0) ); // 123.456.789.0
 
     return 0;
 }
